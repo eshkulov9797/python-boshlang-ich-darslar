15403c4bc65e266ea94aba56abb8d88edbbe69ee
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-18 15:04:18 +0500

    Update dictionary-2.py
    answer-2

--- dictionary-2.py
+++ dictionary-2.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 dictionary = {'int':'butun son','for':'takrorlanish sikli','if':'shart operatori','else':'yoki shart operatori','def':'funksiya'}
 
-for key,value in sorted(dictionary.items()):
-    print(f"{key} - {value}")
+#for key,value in sorted(dictionary.items()):
+#   print(f"{key} - {value}")
+
+davlatlar = {'O\'zbekiston':'Toshkent','Qozog\'iston':'Astana','Qirg\'iziston':'Boku','Tojikiston':'Dushanbe'}
+
+print("\n Davlat nomlari :::\n")
+for key in davlatlar.keys():
+    print(key)
+
+print("\nPoytaxtlar:::\n")
+for poytaxt in davlatlar.values():
+    print(poytaxt)
