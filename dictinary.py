e6436f069f8832038bea6fa2c4eeb3deb48e9225
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-18 10:30:19 +0500

    Update dictinary.py
    2 answer

--- dictinary.py
+++ dictinary.py
@@ -2,8 +2,17 @@
 
 
 otam = {'isim':"Timur",'familiya':"Turdibekov",'sharif':'Doniyorov','yil':1997}
-print(f"{otam['familiya'].title()}")
-print(f"{otam['isim'].title()}")
-print(f"{otam['sharif'].title()}")
-print(f"{otam['yil']} yil")
+#print(f"{otam['familiya'].title()}")
+#print(f"{otam['isim'].title()}")
+#print(f"{otam['sharif'].title()}")
+#print(f"{otam['yil']} yil")
+
+lugat = {'int':'butun sonlar','double':'xaqiqiy sonlar','str':'satr qatorlari','bool':'shart operatori','import':'yuklamoq'}
+print(lugat['int'])
+print(lugat['double'])
+print(lugat['str'])
+print(lugat['bool'])
+
+
+
 
