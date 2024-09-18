bdc607b8888768f4888d2bf0cafa09f9b59a4e6f
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-18 11:22:36 +0500

    Update dictinary.py
    4 answer

--- dictinary.py
+++ dictinary.py
@@ -7,12 +7,20 @@ otam = {'isim':"Timur",'familiya':"Turdibekov",'sharif':'Doniyorov','yil':1997}
 #print(f"{otam['sharif'].title()}")
 #print(f"{otam['yil']} yil")
 
-lugat = {'int':'butun sonlar','double':'xaqiqiy sonlar','str':'satr qatorlari','bool':'shart operatori','import':'yuklamoq'}
-print(lugat['int'])
-print(lugat['double'])
-print(lugat['str'])
-print(lugat['bool'])
+#lugat = {'int':'butun sonlar','double':'xaqiqiy sonlar','str':'satr qatorlari','bool':'shart operatori','import':'yuklamoq'}
+#print(lugat['int'])
+#print(lugat['double'])
+#print(lugat['str'])
+#print(lugat['bool'])
 
+sozlar = {'apple':'olma','water':'suv','mother':'ona','father':'ota','brother':'aka','sister':'opa','yellow':'sariq'}
 
+soz = input("so'zni kiriting : ")
 
+tarjima = sozlar.get(soz)
+if tarjima == None:
+    print("bunday so'z yo'q!!!")
+else:
+    print(f"{soz.title()} tarjimasi {tarjima.title()}")
+    
 
