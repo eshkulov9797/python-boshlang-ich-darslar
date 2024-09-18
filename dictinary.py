77dcd773126d200a06b30a5bda8592c27c49d2cf
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-18 10:22:15 +0500

    Create dictinary.py
    1 answer

---
+++ dictinary.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+otam = {'isim':"Timur",'familiya':"Turdibekov",'sharif':'Doniyorov','yil':1997}
+print(f"{otam['familiya'].title()}")
+print(f"{otam['isim'].title()}")
+print(f"{otam['sharif'].title()}")
+print(f"{otam['yil']} yil")
+
