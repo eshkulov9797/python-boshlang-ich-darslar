bf3f02cd56a3669e9f0cdc97e104ecdac0107027
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-19 15:23:08 +0500

    Update dictionary-3.py
    4 - answer

--- dictionary-3.py
+++ dictionary-3.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
 
-shaxs1 = {'isim':'pushkin','yonalish':'adabiyot','tugulgan joy':'Rassiya'}
-shaxs2 = {'isim':'Amir Temur','yonalish':'Jangovar Sanat','tugulgan joy':'Qashqadaryo'}
-shaxs3 = {'isim':'Ilon Mask','yonalish':'Axborot Texnologiyalari','tugulgan joy':'Amerika Qoshma SHtatlari'}
-shaxs4 = {'isim':'Jorj Burj','yonalish':'prizdent','tugulgan joy':'Angiliya'}
+
+#kinolar = {'ali':['terminator','batman','yulduzlar jangi'],
+ #          'vali':['imperiya','quyosh','saroy'],
+  #         'anton':['rus qoshini','parovoz','xarbiy oila']}
+#
+#for isim,kino in kinolar.items():
+ #   print(f"{isim}ning sevimli kinolari : ", end='')
+  #  for element in kino:
+   #     print(f"{element} ",end='')
+    #print("")
+    
+
+davlatlar = {'uzbekistan':{'poytaxti':'toshkent','aholisi':'31 mln','pul birligi':'som'},
+             'qozogiston':{'poytaxti':'astana','aholisi':'35 mln','pul birligi':'tenge'},
+             'turkmaniston':{'poytaxti':'ashgabat','aholisi':'12 mln','pul birligi':'dinor'},
+             'Qatar':{'poytaxti':'doha','aholisi':'5 mln','pul birligi':'dinor'}}
+
+davlat = input("Davlat nomini kiriting : ")
 
 
-shaxslar = [shaxs1,shaxs2,shaxs3,shaxs4]
-
-for element in shaxslar:
-    print(element['isim'])
-    print(element['yonalish'])
-    print(element['tugulgan joy'])
-    print("---------------------------")
-
+for nomi,element in davlatlar.items():
+    if davlat == nomi:    
+        print(f"{nomi.title()} davlati xaqida batafsil malumot : ")
+        print(f"poytaxti : {element['poytaxti']}")
+        print(f"aholisi : {element['aholisi']}")
+        print(f"pul birligi : {element['pul birligi']}")
+        print("--------------------------------------") 
+        
+    
+        
+        
+    
+    
