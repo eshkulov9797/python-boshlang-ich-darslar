d2d43aabab0113542eb68e799fce6a30f656fcfc
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-19 14:22:52 +0500

    Create dictionary-3.py
    answer-3

---
+++ dictionary-3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+shaxs1 = {'isim':'pushkin','yonalish':'adabiyot','tugulgan joy':'Rassiya'}
+shaxs2 = {'isim':'Amir Temur','yonalish':'Jangovar Sanat','tugulgan joy':'Qashqadaryo'}
+shaxs3 = {'isim':'Ilon Mask','yonalish':'Axborot Texnologiyalari','tugulgan joy':'Amerika Qoshma SHtatlari'}
+shaxs4 = {'isim':'Jorj Burj','yonalish':'prizdent','tugulgan joy':'Angiliya'}
+
+
+shaxslar = [shaxs1,shaxs2,shaxs3,shaxs4]
+
+for element in shaxslar:
+    print(element['isim'])
+    print(element['yonalish'])
+    print(element['tugulgan joy'])
+    print("---------------------------")
+
