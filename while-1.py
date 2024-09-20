7b6b7d8103f007952256c6eb303c7141ff734e71
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-20 10:28:54 +0500

    Create while-1.py
    while 1 - answer

---
+++ while-1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 09:15:00 2024
+
+@author: Root
+"""
+soroq = "Siz qanaqa kitoblar o'qishni yoqtirasiz"
+
+savol = ''
+
+while savol != 'stop':
+    savol = input(soroq)
+    print(f"Siz {savol} ushbu kitobni yoqtirasiz !!!")
+
+print("dastur yakunlandi.")
+    
+
