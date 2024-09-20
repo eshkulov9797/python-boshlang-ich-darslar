fc9bca99cbd0157048c1bf2b55e11949924c6588
eshkulov9797 <138809953+eshkulov9797@users.noreply.github.com>
2024-09-20 16:43:21 +0500

    Update while-1.py
    answer-1

--- while-1.py
+++ while-1.py
@@ -4,14 +4,26 @@ Created on Fri Sep 20 09:15:00 2024
 
 @author: Root
 """
-soroq = "Siz qanaqa kitoblar o'qishni yoqtirasiz"
 
-savol = ''
+taklif = "Salom xush kelibsiz bizning muzeyimizga !!!"
+shart = True
 
-while savol != 'stop':
-    savol = input(soroq)
-    print(f"Siz {savol} ushbu kitobni yoqtirasiz !!!")
-
-print("dastur yakunlandi.")
+while shart:
+    print(taklif)
+    yosh = input("yoshingizni kiriting : ")
+    if yosh == 'exit' or yosh == 'quite':
+        break
+    else:
+        yosh = int(yosh)
     
-
+        
+    if yosh >= 0 and yosh < 7:
+        print(f"Sizning yoshingiz {yosh} sizning kirishingiz uchun 2000 so'm chipta narxi")
+    elif yosh>=7 and yosh < 18:
+        print(f"Sizning yoshingiz {yosh} sizning kirishingiz uchun 3000 so'm chipta narxi")
+    elif yosh>=18 and yosh < 65:
+        print(f"Sizning yoshingiz {yosh} sizning kirishingiz uchun 10000 so'm chipta narxi")
+    elif yosh>=65:
+        print(f"Sizning yoshingiz {yosh} siz bepul kirishingiz mumkin !!!")
+    
+    
